aafee3e18d45aeda5cd4ab66e850fbf1345f6584
Andrés Terol Sánchez <andrests78@gmail.com>
2025-01-17 19:56:34 +0100

    Test facade youtube video

--- src/js/app.js
+++ src/js/app.js
@@ -13,3 +13,13 @@ window.addEventListener('resize', () => {
   const main = document.querySelector('.main-content');
   main.style.marginTop = `${header.offsetHeight}px`;
 });
+
+function loadVideo(placeholder, urlVideo) {
+  const iframe = document.createElement('iframe');
+  iframe.src = urlVideo;
+  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+  iframe.allowFullscreen = true;
+  iframe.style.width = "100%";
+  iframe.style.height= placeholder.height+'px';
+  placeholder.parentNode.replaceChild(iframe, placeholder);
+}
